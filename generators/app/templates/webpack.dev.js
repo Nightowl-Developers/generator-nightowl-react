04ed7cd5b1f8aab7ecdc1572159eff598fbce450
Joshua Blevins <josh.adam.blevins@gmail.com>
2020-02-27 21:40:15 -0500

    added configurations

--- generators/app/templates/webpack.dev.js
+++ generators/app/templates/webpack.dev.js
@@ -1,5 +1,52 @@
-
+const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-
-}
+  mode: "development",
+  target: "web",
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devtool: "inline-source-map",
+  plugins: [
+    new webpack.debug.ProfilingPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jp?g|svg|gif)$/i,
+        use: {
+          loader: 'file-loader',
+        }
+      },
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ["css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    host: "localhost",
+    port: 3000,
+    contentBase: path.join(__dirname, "build"),
+    filename: "bundle.js",
+    historyApiFallback: true,
+    hot: true
+  }
+};
