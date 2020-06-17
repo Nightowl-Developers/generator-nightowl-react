eea295dae233352d46b1bad4139fb27d9bc9d7c4
Joshua Blevins <josh.adam.blevins@gmail.com>
2020-06-17 12:23:29 -0400

    refactored component to functional page

---
+++ generators/component/templates/typescript/page.ts
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+interface <%= name %>Props {};
+
+const <%= name %> = (props): React.FunctionalComponentElement<<%= name %>Props> => {
+    return <div>
+        <p>Functional Component</p>
+    </div>;
+};
+
+export default <%= name %>;
