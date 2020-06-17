31205572f5cfb68420fbd95ddae41c7ee57e8992
Joshua Blevins <josh.adam.blevins@gmail.com>
2020-06-17 12:21:38 -0400

    refactored componentjs to functional component

---
+++ generators/component/templates/es6/page.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const <%= name %> = (props) => {
+    return <div>
+        <p>Functional Component</p>
+    </div>;
+};
+
+export default <%= name %>;
